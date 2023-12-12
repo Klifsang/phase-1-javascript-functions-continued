c80d5839bd357f27984b108b873dac8f75098eca
Klif <sang.wicklife@student.moringaschool.com>
2023-12-13 01:13:41 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+function saturdayFun(params) {
+    switch (params) {
+        case "bathe my dog":
+            return "This Saturday, I want to bathe my dog!";
+            break;
+    
+        default:
+            return "This Saturday, I want to roller-skate!";
+            break;
+    }
+}
+
+function mondayWork(params) {
+    switch (params) {
+        case "work from home":
+            return "This Monday, I will work from home.";
+            break;
+    
+        default:
+            return "This Monday, I will go to the office.";
+            break;
+    }
+}
+
+function wrapAdjective(wrapper) {
+    const adj = function(adjString){
+        return "You are "+wrapper+adjString+wrapper+"!";
+    }
+    return adj;
+}
